64027fda02b6e2ec97cde17bad33c8343030f6d6
Poojith Patnala (patnalap) <patnalap@coventry.ac.uk>
2022-07-21 16:55:48 +0100

    Edited url to output Celsius units for temperature rather than Kelvin

--- single.py
+++ single.py
@@ -5,7 +5,7 @@ class Single():
 
     def __init__(self, cityname):
         self.url= "http://api.openweathermap.org/data/2.5/" \
-                  "weather?appid=73041764461c75168f67f580c696f3a4&q="+cityname #own API key
+                  "weather?appid=73041764461c75168f67f580c696f3a4&q="+cityname+ "&units=metric" #own API key
 
         self.request = requests.get(self.url) #request will contain all the response codes
         self.resp_json = self.request.json() #uses the request # should contain a dictionary
