5906a8cd830f8ab98546767b5ab80def147a5b61
Poojith Patnala (patnalap) <patnalap@coventry.ac.uk>
2022-07-21 17:13:54 +0100

    More information outputted to the user - takes in user input through terminal

--- services.py
+++ services.py
@@ -3,9 +3,22 @@ import requests
 from single import Single
 
 
-location = Single("London")
-print(location)
+user_location= input("Enter the location: ")
+location = Single(user_location)
+
+print(location.response_data().name)
+#print(location.response_data().description)
+print(location.response_data().temps)
+
+print(location.response_data().humidity)
+print(location.response_data().wind_speed)
 
 print(location.response_data().coord)
+
+
+
 print(location.response_data().main)
+print(location.response_data().temps_max)
+print(location.response_data().temps_min)
+#print(location.response_data())
 
--- single_model.py
+++ single_model.py
@@ -3,6 +3,20 @@
 class SingleModel:
 
     def __init__(self, single_location): # constructor
+        self.name = single_location['name'] #location name
+
         self.main = single_location['main']
-        self.coord = single_location['coord']
+        self.temps = self.main['temp'] #Current temp
+        self.temps_min = self.main['temp_min'] #Min temp for the day
+        self.temps_max = self.main['temp_max'] #Max temp for the day
+        self.humidity = self.main['humidity'] #Humidity of the day
+
+        self.weather = single_location['weather']
+        #self.description = self.weather['description'] #Current Weather description
+
+        self.wind = single_location['wind']
+        self.wind_speed = self.wind['speed'] #Wind speed
+
+        self.coord = single_location['coord'] #latitude and longitude of location
+
 
