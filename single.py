4aab55bf35c55a6075df49be70df91dc4e9141b4
Poojith Patnala (patnalap) <patnalap@coventry.ac.uk>
2022-07-21 16:43:26 +0100

    Added comments to the functions

--- single.py
+++ single.py
@@ -5,11 +5,15 @@ class Single():
 
     def __init__(self, cityname):
         self.url= "http://api.openweathermap.org/data/2.5/" \
-                  "weather?appid=73041764461c75168f67f580c696f3a4&q="+cityname
+                  "weather?appid=73041764461c75168f67f580c696f3a4&q="+cityname #own API key
 
-        self.request = requests.get(self.url)
-        self.resp_json = self.request.json()
+        self.request = requests.get(self.url) #request will contain all the response codes
+        self.resp_json = self.request.json() #uses the request # should contain a dictionary
 
     def response_data(self):
-        return SingleModel(self.resp_json)
+        '''
+        Utilised the data model to create the object
+        :return: returns the corresponding values from json format
+        '''
+        return SingleModel(self.resp_json) # utilise the data model to create the object
 
