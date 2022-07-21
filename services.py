544122b243e45009ab211d580bd339fdc8caaa2e
Poojith Patnala (patnalap) <patnalap@coventry.ac.uk>
2022-07-21 17:23:46 +0100

    Formatted the output results to a more readable format

--- services.py
+++ services.py
@@ -6,19 +6,28 @@ from single import Single
 user_location= input("Enter the location: ")
 location = Single(user_location)
 
-print(location.response_data().name)
+print("\n")
+print("Current City: "+location.response_data().name)
 #print(location.response_data().description)
+print("Current temperature in Celsius:")
 print(location.response_data().temps)
 
+print("Humidity: ")
 print(location.response_data().humidity)
+
+print("Wind Speed: ")
 print(location.response_data().wind_speed)
 
+print("\n")
+print("Location Coordinates (lon, lat) :")
 print(location.response_data().coord)
 
+print("\n")
+print("Additional details: ")
 
-
-print(location.response_data().main)
+print("Max temperature for today in Celsius:")
 print(location.response_data().temps_max)
+print("Min temperature for today in Celsius:")
 print(location.response_data().temps_min)
-#print(location.response_data())
+
 
